8cddce1af4a8225dc01237045790c321d1d5d78e
Rajat Sharma <rajat@developar.in>
2021-12-08 15:26:27 +0530

    Added pageAction

--- js/page/render.js
+++ js/page/render.js
@@ -7,14 +7,23 @@
          .forEach(function(el) {
             name = el.getAttribute('data-page-icon');
             source = window['icons'][name];
-      
+
             if (typeof source === 'string') {
                el.innerHTML = source;
+               el.removeAttribute('data-page-icon');
             }
          });
    }
 
    if (window['icons']) {
       _renderPageIcons();
+   }
+
+   window['pageAction'] = function (actionName = 'NONE') {
+      switch(actionName) {
+         case 'RENDER_SOCIAL_ICONS':
+            _renderPageIcons();
+            break;
+      }
    }
 })();
